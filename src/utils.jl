e91b58111d66b231fae1a5581b3d2b75bd09e6b7
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-10-31 10:06:39 +0100

    Rt2T etc. return static arrays

--- src/utils.jl
+++ src/utils.jl
@@ -1,10 +1,13 @@
-Rt2T(R,t) = [R t; 0 0 0 1]
-T2R(T::AbstractMatrix) = T[1:3,1:3]
-T2R(T::SMatrix) = SA[   T[1,1] T[1,2] T[1,3]
+Rt2T(R,t) = SA[   R[1,1] R[1,2] R[1,3] t[1]
+                        R[2,1] R[2,2] R[2,3] t[2]
+                        R[3,1] R[3,2] R[3,3] t[3]
+                        0      0      0      1]
+# T2R(T::AbstractMatrix) = T[1:3,1:3]
+T2R(T::AbstractMatrix) = SA[   T[1,1] T[1,2] T[1,3]
                         T[2,1] T[2,2] T[2,3]
                         T[3,1] T[3,2] T[3,3]]
-T2t(T::AbstractMatrix) = T[1:3,4]
-T2t(T::SMatrix) = SVector(T[1,4],T[2,4],T[3,4])
+# T2t(T::AbstractMatrix) = T[1:3,4]
+T2t(T::AbstractMatrix) = SVector(T[1,4],T[2,4],T[3,4])
 t2T(t::AbstractVector{T}) where T = SA[ 1 0 0 t[1]
                                         0 1 0 t[2]
                                         0 0 1 t[3]
